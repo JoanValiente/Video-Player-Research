1cde4c3df708157ad95f72f0890b01bbc2ad8769
JoanValiente <joan.valiente.lorite@gmail.com>
2018-05-03 18:30:45 +0200

    separated full code from exercices

---
+++ full code/Video Player/Motor2D/Defs.h
@@ -0,0 +1,76 @@
+#ifndef __DEFS_H__
+#define __DEFS_H__
+
+#include <stdio.h>
+
+// SDL colors
+#define ColorBlack { 0,0,0,255 }
+#define ColorWhite { 255,255,255,255 }
+#define ColorRed { 255,0,0,255 }
+#define ColorGreen { 0,255,0,255 }
+#define ColorBlue { 0,0,255,255 }
+
+/// Units (max units)
+#define ColorYellow { 255,255,0,255 }
+#define ColorDarkGreen { 0,102,0,255 }
+#define ColorBrightBlue { 0,255,255,255 }
+#define ColorOrange { 255,128,0,255 }
+#define ColorPink { 255,0,127,255 }
+#define ColorPurple { 127,0,255,255 }
+#define ColorGrey { 128,128,128,255 }
+#define ColorOlive { 102,204,0,255 }
+#define ColorViolet { 178,102,255,255 }
+
+/// Colliders
+#define ColorDarkBlue { 0,0,153,255 }
+#define ColorLightBlue { 102,178,255,255 }
+#define ColorDarkRed { 153,0,0,255 }
+#define ColorLightRed { 255,102,102,255 }
+
+//  NULL just in case ----------------------
+
+#ifdef NULL
+#undef NULL
+#endif
+#define NULL 0
+
+// Deletes a buffer
+#define RELEASE( x ) \
+    {                        \
+    if( x != NULL )        \
+	    {                      \
+      delete x;            \
+	  x = NULL;              \
+	    }                      \
+    }
+
+// Deletes an array of buffers
+#define RELEASE_ARRAY( x ) \
+    {                              \
+    if( x != NULL )              \
+	    {                            \
+      delete[] x;                \
+	  x = NULL;                    \
+	    }                            \
+                              \
+    }
+
+#define IN_RANGE( value, min, max ) ( ((value) >= (min) && (value) <= (max)) ? 1 : 0 )
+#define MIN( a, b ) ( ((a) < (b)) ? (a) : (b) )
+#define MAX( a, b ) ( ((a) > (b)) ? (a) : (b) )
+#define TO_BOOL( a )  ( (a != 0) ? true : false )
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+typedef unsigned long long uint64;
+typedef unsigned long uint32;
+
+// Standard string size
+#define SHORT_STR	32
+#define MID_STR		255
+#define HUGE_STR	8192
+
+
+
+#endif //__DEFS_H__
---
+++ full code/Video Player/Motor2D/j1Scene.cpp
@@ -0,0 +1,79 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Audio.h"
+#include "j1Render.h"
+#include "j1Window.h"
+#include "j1Map.h"
+#include "j1Scene.h"
+#include "j1Video.h"
+#include "SDL_mixer\include\SDL_mixer.h"
+
+j1Scene::j1Scene() : j1Module()
+{
+	name.create("scene");
+}
+
+// Destructor
+j1Scene::~j1Scene()
+{}
+
+// Called before render is available
+bool j1Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1Scene::Start()
+{
+	App->video->Initialize("video/sample(good).avi");
+	App->audio->PlayMusic("video/sample.ogg", 0.0f);
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PreUpdate()
+{
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::Update(float dt)
+{
+	if (!App->video->isVideoFinished)
+	{
+		App->video->GrabAVIFrame();
+
+	}
+	else 
+		Mix_PauseMusic();
+
+
+	if (App->input->GetKey(SDL_SCANCODE_1) && App->video->isVideoFinished) {
+		App->video->Initialize("video/sample(good).avi");
+		App->audio->PlayMusic("video/sample.ogg", 0.0f);
+	}
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PostUpdate()
+{
+	bool ret = true;
+	return ret;
+}
+
+// Called before quitting
+bool j1Scene::CleanUp()
+{
+	App->video->CloseAVI();
+	return true;
+}
